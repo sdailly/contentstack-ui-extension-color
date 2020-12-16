8ee575571330469d97e61247c89992e3a2b24374
DAILLY <stephane.dailly@promod.fr>
2020-12-16 14:33:37 +0100

    delete unused files

--- tests/__mocks__/axios.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-undef
-const mockAxios = jest.genMockFromModule('axios')
-
-// eslint-disable-next-line no-undef
-mockAxios.create = jest.fn(() => mockAxios)
-
-export default mockAxios
